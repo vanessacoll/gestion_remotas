7741c53e11077711cb09cfae39ff03980c791e47
vanessacoll <collvanessa041@gmail.com>
2023-03-12 00:28:41 -0400

    actualizacion del login

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ class User extends Authenticatable
     
     use HasFactory, Notifiable;
 
+    protected $primaryKey = 'id';
+
       /**
      * The attributes that are mass assignable.
      *
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,113 +1,22 @@
+@extends('themes.login.login_template')
 
-<!DOCTYPE html>
-<html>
-<!-- inicio head-->
-  @include("themes/$theme/head")
-  <!-- fin head-->
-<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-<body class="hold-transition skin-blue layout-top-nav">
-<div class="wrapper">
+@section('form')
 
-  <header class="main-header">
-    <nav class="navbar navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a href="/home" ><img src="{{asset("assets/$theme/dist/img/PhonettMail.png")}}" alt="phonnet Image"></a>
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-            <i class="fa fa-bars"></i>
-          </button>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-       
-        </div>
-        <!-- /.navbar-collapse -->
-
-        <!-- /.navbar-custom-menu -->
-      </div>
-      <!-- /.container-fluid -->
-    </nav>
-  </header>
-  <!-- Full Width Column -->
-  <div class="content-wrapper">
-   <section class="content">
-<div class="row justify-content-center">
-        <!-- left column -->
-        <div class="col-md-6">
-          <!-- general form elements -->
-          <div class="box box-primary">
-            <div class="box-header with-border">
-              <h3 class="box-title">Login</h3>
-            </div>
-            <!-- /.box-header -->
-   <form method="POST" action="{{ route('login') }}">
-                        @csrf
-<div class="box-body">
-  <div class="row">
-    <div class="col-md-12">
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-      </div>
-    </div>
-  </div>                    
-
-              <div class="box-footer">
-          <div class="row justify-content-center">
-                       <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Iniciar sesión') }}
-                                </button>
-
-                            </div>
-                        </div>
-                      </div>
-              </div>
-                    </form>
-    </div>
-          <!-- /.box -->
+<form method="POST" action="{{ route('login') }}">
+  @csrf
+  <div class="illustration">
+    <img src="{{ asset('/assets/images/logo.png') }}" alt="" style="height: 100px">
   </div>
-    </div>
-    
-    </section>
-    <!-- /.container -->
+  <div class="form-group">
+    <input class="form-control" type="text" id="username" name="username" placeholder="Usuario">
   </div>
-  <!-- /.content-wrapper -->
-  <!--inicio footer-->
-  @include("themes/$theme/footer")
-<!-- fin footer -->
-</div>
-<!-- ./wrapper -->
+  <div class="form-group">
+    <input class="form-control" type="password" id="password" name="password" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <button class="btn btn-primary btn-block" type="submit">Iniciar Sesion</button>
+  </div>
+</form>
 
-<!-- inicio jQuery 3 -->
-  @include("themes/$theme/jquery")
-<!-- fin jQuery 3 -->
-</body>
-</html>
+@endsection
+
